08a3a144b5c0c6c68fae4c1fddb9002c7934a979
相甫 <xiangfu_git@163.com>
2024-02-02 20:57:20 +0800

    feat: 🎸 react-native-config集成

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,10 @@
 import { ArrowDownFill } from '@fruits-chain/icons-react-native'
 import { Toast } from '@fruits-chain/react-native-xiaoshu'
 import { Button, Text, View } from 'react-native'
+import RNConfig from 'react-native-config'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 import { navigate } from '@/router/root-navigation'
-
 const Home: React.FC = () => {
   const insets = useSafeAreaInsets()
 
@@ -12,10 +12,12 @@ const Home: React.FC = () => {
     <>
       <View style={{ height: insets.top }} />
       <Text>Home</Text>
+      <Text>{RNConfig.HOST}</Text>
       <ArrowDownFill
         color="#098"
         onPress={() => {
           Toast('你点击了我')
+          console.log('🚀🚀🚀wimi======>>>RNConfig.HOST', RNConfig.HOST)
         }}
       />
       <Button
